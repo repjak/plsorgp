9fcd92a528eb8c8e74a115b3599f12b2ca7a4884
jakub <j.repicky@gmail.com>
2016-11-11 16:14:02 +0100

    Fix gallery call.

--- unittest/CholAlgTest.m
+++ unittest/CholAlgTest.m
@@ -4,18 +4,18 @@ classdef CholAlgTest < matlab.unittest.TestCase
 
   methods (Test)
     function cholInvTest(testCase)
-      for type = {'lehmer', 'minij'};
+      for type = {'lehmer', 'minij'}
         for n = [0 1 2 5 10 20 100 500 1000 2000]
-          A = gallery('lehmer', n);
+          A = gallery(type{:}, n);
           R = chol(A);
           Ainv = cholinv(R);
-          testCase.verifyEqual(A*Ainv, eye(n), 'AbsTol', 1e-8);
+          testCase.verifyEqual(A*Ainv, eye(n), 'AbsTol', 1e-4);
 
           X = sqrt(n)*randn(n, n);
           Y = cholsolve(R, A*X);
           Z = cholsolve(R, X*A, 'right');
-          testCase.verifyEqual(Y, X, 'AbsTol', 1e-8);
-          testCase.verifyEqual(Z, X, 'AbsTol', 1e-8);
+          testCase.verifyEqual(Y, X, 'AbsTol', 1e-4);
+          testCase.verifyEqual(Z, X, 'AbsTol', 1e-4);
         end
       end
     end
