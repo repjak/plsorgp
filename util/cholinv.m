62006684981c9d992953f82dffa361d4dcfa955e
jakub <j.repicky@gmail.com>
2016-11-15 20:39:09 +0100

    Help strings.

--- util/cholinv.m
+++ util/cholinv.m
@@ -4,6 +4,8 @@ function Ainv = cholinv(R)
 %   Ainv = cholinv(R) compute the inverse of matrix A with Cholesky
 %   decomposition A=R'R by solving the following systems of linear
 %   equations: RX = B, R'B = I.
+%
+%   See also CHOLSOLVE.
 
   Ainv = R \ (R' \ eye(size(R, 1)));
 end
--- util/cholsolve.m
+++ util/cholsolve.m
@@ -3,6 +3,8 @@ function X = cholsolve(R, B, varargin)
 %its Cholesky decomposition, i.e. A = R'R.
 %   X = cholsolve(R, B) solve AX = B for X, where A = R'R
 %   X = cholsolve(R, 'right') solve XA = B for X, where A = RR'
+%
+%   See also CHOLINV.
 
   if nargin >= 3 && strcmp(varargin{1}, 'right')
     X = (B / R) / R';
