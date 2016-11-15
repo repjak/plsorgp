d5253b262d135177ae170a15cc97d5ecee656296
jakub <j.repicky@gmail.com>
2016-11-15 20:13:35 +0100

    Refactoring. A custom "multistart" optimization.

--- OrdRegressionGP.m
+++ OrdRegressionGP.m
@@ -1,37 +1,64 @@
-classdef OrdRegressionGP
+classdef OrdRegressionGP < handle
   %ORDREGRESSIONGP An Ordinal Regression Gaussian Process model.
 
-  properties (GetAccess = protected, SetAccess = private)
-    X                   % the predictors
-    y                   % user-supplied response values
+  properties (GetAccess = protected, SetAccess = private, Hidden = true)
     muX = 0             % predictors mean for standardization
     stdX = 1            % predictors std for standardization
     n                   % the number of observations
     r                   % the number of ordinal classes
-    d                   % the dimension of the input space
+    d                   % the dimensionality of the input space
     yUnq                % uniquely sorted response values
     yUnqIdx             % indices of the sorted response values
     ys                  % response values transformed into classes 1:r
     covFcn              % the covariance function
     hyp = struct()      % a structure of model's hyperparameters
-    nHyp                % the number of the covariance fcn's hyperparameters
-    nlp = Inf           % the optimized negative log probability
-    optRes = struct()   % a structure of optimization results
-    lossFcn = @zeroone  % a loss function for predictions
+    nHyp                % hyperparameters total
+    nHypCov             % the number of the covariance fcn's hyperparameters
+    nlpFcn              % the negative log probability (a pseudo likelihood)
+    optRes              % a structure of optimization results
+    lossFcn             % a loss function for predictions
+    fitMethod = 'exact' % the fitting method, either 'exact' or 'none'
+    standardize = true  % standardize the training data
+    optimopts           % optimizer options
+    minNlp = Inf        % the optimal negative log probability
     K                   % the covariance matrix on the training data
     R                   % the upper-triangular cholesky factor of the covariance matrix
     Kinvy               % (K + sigma2 * eye(n)) \ y
   end
 
+  properties(GetAccess = public, SetAccess = private)
+    X                        % the training predictors
+    y                        % the training response values
+    NumObservations          % the number of observations
+    NumClasses               % the number of ordinal classes
+    Dimensionality           % the dimensionality of the input space
+    Standardize = true       % standardize the training data
+    FitMethod                % the fitting method, either 'exact' or 'none'
+    CrossVal                 % the crossvalidation partitiong, 'leave1out'
+    LossFunction             % the loss function for deciding prediction
+    KernelFunction           % the GP kernel function
+    KernelParameters         % the hyperparameters of the GP kernel
+    PlsorParameters          % the hyperparameters of the PLSOR method
+    OptimizerOptions         % optimizer options
+    Sigma2                   % the GP's Gaussian noise variance
+    OptimInfo                % optimizer's result info
+    OptimExitFlag            % optimizer's exit flag
+    OptimTrial               % the best starting point if multistart is used
+    MinimumNLP               % optimized value of negative log probability
+  end
+
   methods
-    function model = OrdRegressionGP(data, resp, varargin)
-      %ORDREGRESSIONGP Fit a Probabilistic Least Squares Ordinal Regression Gaussian
-      %Process model.
+    function obj = OrdRegressionGP(data, resp, varargin)
+      %ORDREGRESSIONGP Fit a Probabilistic Least Squares Ordinal Regression
+      %Gaussian Process model.
       %   model = ORDREGRESSIONGP(tbl, ResponseVarName)
       %   model = ORDREGRESSIONGP(tbl, formula)
       %   model = ORDREGRESSIONGP(tbl, y)
       %   model = ORDREGRESSIONGP(X, y)
-      %   model = ORDREGRESSIONGP(__, Name, Value)
+      %   model = ORDREGRESSIONGP(__, Name, Value) specify Name-Value pairs
+      %   using any of the previous syntaxes.
+      %   model = ORDREGRESSIONGP(__, args) specify Name-Value pairs in
+      %   a cell array.
 
       if nargin == 0
         return;
@@ -41,29 +68,37 @@ classdef OrdRegressionGP
       p = inputParser;
       p.addRequired('data', @(x) ~isempty(x) && (istable(x) || isnumeric(x)));
       p.addRequired('resp', ...
-        @(x) ~isempty(x) && (ischar(x) || (isint(x) && size(x,2) == 1)));
+        @(x) ~isempty(x) && (ischar(x) || (isnumeric(x) && size(x,2) == 1)));
 
       p.addParameter('CrossVal', 'leave1out', ...
         @(x) validateattributes({'char'}, {'leave1out'}));
-      p.addParameter('Standardize',      true, ...
+      p.addParameter('Standardize', true, ...
         @(x) validateattributes({'logical'}, {'size', [1, 1]}));
       covFcns = {'squaredexponential', 'ardsquaredexponential'};
       p.addParameter('KernelFunction', 'ardsquaredexponential', ...
-        @(x) (ischar(x) && ismember(x, covFcns)) || isa(x, 'function_handle'));
+        @(x) (ischar(x) && ismember(x, covFcns)) || ...
+        isa(x, 'function_handle'));
       p.addParameter('KernelParameters', [], ...
         @(x) validateattributes({'numeric'}, {'nonempty'}));
       p.addParameter('PlsorParameters', [], ...
         @(x) validateattributes({'numeric'}, {'nonempty'}));
-      p.addParameter('FitMethod', ...
+      p.addParameter('FitMethod', 'exact', ...
         @(x) validateattributes({'char'}, {'none', 'exact'}));
       p.addParameter('Sigma2', 0, ...
         @(x) validateattributes({'double'}, {'positive'}));
       p.addParameter('OptimizerOptions', [], ...
         @(x) isa(x, 'optim.options.Fmincon'));
       p.addParameter('LossFunction', 'zeroone', ...
-        @(x) validateattributes({'char'}, {'zeroone', 'hinge'}));
+        @(x) (ischar(x) && ismember(x, {'zeroone', 'hinge'})) || ...
+        isa(x, 'function_handle'));
 
-      p.parse(data, resp, varargin{:});
+      if nargin == 3 && iscell(varargin{3})
+        arglist = varargin{3};
+      else
+        arglist = varargin;
+      end
+
+      p.parse(data, resp, arglist{:});
 
       % extract data
       if istable(p.Results.data)
@@ -98,15 +133,16 @@ classdef OrdRegressionGP
         obj.muX = mean(obj.X);
         obj.stdX = std(obj.X);
         obj.X = bsxfun(@minus, obj.X, obj.muX);
-        obj.X = bsxfun(@divide, obj.X, obj.stdX);
+        obj.X = bsxfun(@rdivide, obj.X, obj.stdX);
       end
+      obj.standardize = p.Results.Standardize;
 
       % determine ordinal classes
-      [obj.yUnq, obj.yUnqIdx, obj.ys] = unique(y, 'sorted');
-      obj.r = length(unique(ys, 'sorted'));
+      [obj.yUnq, obj.yUnqIdx, obj.ys] = unique(obj.y, 'sorted');
+      obj.r = length(unique(obj.ys, 'sorted'));
 
-      obj.n = size(X, 1);
-      obj.d = size(X, 2);
+      obj.n = size(obj.X, 1);
+      obj.d = size(obj.X, 2);
 
       obj.hyp = struct();
       obj.hyp.cov = p.Results.KernelParameters;
@@ -121,7 +157,7 @@ classdef OrdRegressionGP
               error('Kernel function ''%s'' takes 2 hyperparameters.', ...
                 p.Results.KernelFunction);
             elseif isempty(obj.hyp.cov)
-              obj.hyp.cov = [1 1/d];
+              obj.hyp.cov = [1 1/obj.d];
             else
               obj.hyp.cov = reshape(obj.hyp.cov, 1, numel(obj.hyp.cov));
             end
@@ -131,9 +167,9 @@ classdef OrdRegressionGP
               error('Kernel function ''%s'' takes dim + 2 hyperparameters.', ...
                 p.Results.KernelFunction);
             elseif isempty(obj.hyp.cov)
-              hyp.cov = [1 ones(d, 1)/d];
+              obj.hyp.cov = [1 ones(obj.d, 1)/obj.d];
             else
-              hyp.cov = reshape(obj.hyp.cov, 1, numel(obj.hyp.cov));
+              obj.hyp.cov = reshape(obj.hyp.cov, 1, numel(obj.hyp.cov));
             end
           otherwise
             error('Unknown kernel function ''%s''.', p.Results.KernelFunction);
@@ -147,88 +183,80 @@ classdef OrdRegressionGP
         end
       end
 
-      switch p.Results.LossFunction
-        case 'zeroone'
-          obj.lossFcn = @zeroone;
-        case 'hinge'
-          obj.lossFcn = @hinge;
-        otherwise
-          error('Unknown loss function ''%s''.', p.Results.LossFunction);
-      end
+      obj.nHypCov = length(obj.hyp.cov);
 
       % set the noise hyperparameter
       if p.Results.Sigma2 == 0
-        obj.hyp.sigma2 = std(ys) / sqrt(2);
+        obj.hyp.sigma2 = std(obj.ys) / sqrt(2);
       else
         obj.hyp.sigma2 = p.Results.SigmaNoise;
       end
 
       % set default plsor values
-      if isempty(hyp.plsor)
-        alfa = -1;
-        deltas = repmat(ceil(range(ys) / r), 1, r - 2);
-        betas = deltas(1) .* (1:r-2);
-        obj.hyp.plsor = [alfa beta1 betas];
+      if isempty(obj.hyp.plsor)
+        alpha = 1;
+        % deltas = repmat(ceil(range(obj.ys) / obj.r), 1, obj.r - 2);
+        deltas = repmat(2 / obj.r, 1, obj.r - 2);
+        beta1 = -1;
+        obj.hyp.plsor = [alpha beta1 deltas];
       end
 
-      obj.nHyp = length(hyp.cov) + length(hyp.plsor) + 1;
+      obj.nHyp = length(obj.hyp.cov) + length(obj.hyp.plsor) + 1;
+
+      if ischar(p.Results.LossFunction)
+        switch p.Results.LossFunction
+          case 'zeroone'
+            obj.lossFcn = @zeroone;
+          case 'hinge'
+            obj.lossFcn = @hinge;
+          otherwise
+            error('Unknown loss function ''%s''.', p.Results.LossFunction);
+        end
+      else
+        obj.lossFcn = p.Results.LossFunction;
+      end
+
+      obj.fitMethod = p.Results.FitMethod;
 
       switch p.Results.CrossVal
         case 'leave1out'
-          nlp = @(hyp) negLogPredProb(hyp, length(hyp.cov), covFcn, ...
-            @logPredProbLeaveOneOut, X, ys, n);
+          covFcn = obj.covFcn;
+          nHypCov = obj.nHypCov;
+          X = obj.X;
+          y = obj.ys;
+          n = obj.n;
+
+          obj.nlpFcn = @(hyp) negLogPredProb(...
+            hyp, nHypCov, covFcn, @logPredProbLoo, X, y, n);
         otherwise
           error('Cross-validation ''%s'' not supported.', p.Results.CrossVal);
       end
 
-      switch p.Results.FitMethod
-        case 'exact'
-          if isempty(p.Results.OptimizerOptions)
-            optimopts = optimoptions( ...
-              @fmincon, ...
-              'GradObj', 'on' ...
-            );
-          else
-            optimopts = p.Results.OptimizerOptions;
-          end
-
-          optproblem = struct( ...
-            'objective', nlp, ...
-            'x0', [hyp.plsor hyp.cov hyp.sigma], ...
-            'lb', [-Inf -Inf ...
-              eps+[zeros(1, length(hyp.plsor)-2) zeros(1, length(hyp.cov)+1)]], ...
-            'options', optimopts ...
-          );
-
-          [hyp0, p, exitflag, optinfo] = fmincon(optproblem);
-
-           obj.optRes = struct( ...
-            'min', hyp0, ...
-            'minVal', p, ...
-            'exitflag', exitflag, ...
-            'optinfo', optinfo ...
-          );
-
-          obj.hyp.plsor = hyp0(1:length(obj.hyp.plsor));
-          obj.hyp.cov = hyp0(length(obj.hyp.plsor)+1:end-1);
-          obj.hyp.sigma2 = hyp0(end);
-
-          obj.nlp = p;
-        case 'none'
-        otherwise
-          error('Fit method ''%s'' not supported', p.Results.FitMethod);
+      if isempty(p.Results.OptimizerOptions)
+        obj.optimopts = optimoptions( ...
+          @fmincon, ...
+          'GradObj', 'on', ...
+          'Display', 'off' ...
+        );
+      else
+        obj.optimopts = p.Results.OptimizerOptions;
       end
 
+      obj.fit();
+
+      % precompute the covariance matrix for prediction calls
       obj.K = obj.covFcn(obj.X, obj.X, obj.hyp.cov);
       obj.R = chol(obj.K + obj.hyp.sigma2 * eye(n));
-      obj.Kinvy = cholsolve(R, obj.ys);
+      obj.Kinvy = cholsolve(obj.R, obj.ys);
     end
 
-    function [y, p] = predict(obj, Xnew)
+    function [y, p, mu, s2] = predict(obj, Xnew)
       %ORDREGRESSIONGP.PREDICT An ordinal probabilistic prediction.
-      %   [y, p] = ORDREGRESSIONGP.PREDICT(Xnew) return a column vector of
-      %   predicted classes and a column vector of predicted probabilities
-      %   for N-R data Xnew.
+      %   [y, p]         = ORDREGRESSIONGP.PREDICT(Xnew) return a column
+      %   vector of predicted classes and a column vector of predicted
+      %   probabilities for N-R data Xnew.
+      %   [y, p, mu, s2] = ORDREGRESSIONGP.PREDICT(Xnew) return also the
+      %   predictive mean and variance of the latent variable.
 
       if size(Xnew, 2) ~= obj.d
         error('Input dimensionality %d does not agree.', size(Xnew, 2));
@@ -238,6 +266,8 @@ classdef OrdRegressionGP
       Xnew = bsxfun(@minus, Xnew, obj.muX);
       Xnew = bsxfun(@rdivide, Xnew, obj.stdX);
 
+      m = size(Xnew, 1);
+
       % GP prediction assuming Gaussian likelihood
       [mu, s2] = gpPred(obj.X, [], Xnew, obj.covFcn, obj.hyp.cov, ...
         [], obj.R, obj.Kinvy);
@@ -245,7 +275,7 @@ classdef OrdRegressionGP
       % probabilistic predictions for all classes and all test data
       P = zeros(m, obj.r);
       for i = 1:m
-        P(i, :) = predProb(1:obj.r, obj.hyp.plosr, mu(i), s2(i));
+        P(i, :) = predProb(1:obj.r, obj.hyp.plsor, mu(i), s2(i));
       end
 
       [~, idx, predProbs] = obj.lossFcn(P);
@@ -253,6 +283,114 @@ classdef OrdRegressionGP
       % map the predicted ordinal class index back to the input range
       y = obj.yUnq(idx);
       p = predProbs;
+    end
+
+    function hyp = get.KernelParameters(obj)
+      hyp = obj.hyp.cov;
+    end
+
+    function hyp = get.PlsorParameters(obj)
+      hyp = obj.hyp.plsor;
+    end
+
+    function sigma2 = get.Sigma2(obj)
+      sigma2 = obj.hyp.sigma2;
+    end
+
+    function n = get.NumObservations(obj)
+      n = obj.n;
+    end
+
+    function r = get.NumClasses(obj)
+      r = obj.r;
+    end
+
+    function d = get.Dimensionality(obj)
+      d = obj.d;
+    end
+
+    function cv = get.CrossVal(~)
+      cv = 'leave1out';
+    end
+
+    function lossFcn = get.LossFunction(obj)
+      s = functions(obj.lossFcn);
+      lossFcn = s.function;
+    end
+
+    function covFcn = get.KernelFunction(obj)
+      s = functions(obj.covFcn);
+      switch s
+        case 'sqexp'
+          covFcn = 'squaredexponential';
+        case 'sqexpard'
+          covFcn = 'ardsquaredexponential';
+        otherwise
+          covFcn = s.function;
+      end
+    end
+
+    function fitMethod = get.FitMethod(obj)
+      fitMethod = obj.fitMethod;
+    end
+
+    function optimopts = get.OptimizerOptions(obj)
+      optimopts = obj.optimopts;
+    end
+
+    function minNlp = get.MinimumNLP(obj)
+      minNlp = obj.minNlp;
+    end
+  end
+
+  methods (Access = private)
+    function fit(obj)
+      switch obj.fitMethod
+        case 'exact'
+          lb = [-Inf -Inf ...
+            1e-3+[zeros(1, length(obj.hyp.plsor)-2) ...
+            zeros(1, obj.nHypCov + 1)]];
+%          ub = [Inf(1, length(obj.hyp.plsor)) ...
+%            5*std(obj.y) 5];
+
+          startPoints = zeros(3, obj.nHyp);
+          startPoints(1, :) = [obj.hyp.plsor obj.hyp.cov obj.hyp.sigma2];
+
+          startPoints(2, :) = [1 0 ones(1, obj.r - 2) obj.hyp.cov obj.hyp.sigma2];
+
+          b = sort(1.5 * (obj.r - 1) * rand(1, obj.r - 1));
+          alpha = 4 * rand() - 2;
+          beta1 = rand() - 0.5;
+          delta = arrayfun(@(i) b(i) - b(i - 1), 2:(obj.r - 1));
+          startPoints(3, :) = [alpha beta1 delta obj.hyp.cov obj.hyp.sigma2];
+
+          optproblem = struct( ...
+            'solver', 'fmincon', ...
+            'objective', obj.nlpFcn, ...
+            'lb', lb, ...
+            'options', obj.optimopts ...
+          );
+
+          for i = 1:size(startPoints, 1)
+            optproblem.x0 = startPoints(i, :);
+            [minx, miny, exitflag, optinfo] = fmincon(optproblem);
+
+            if miny < obj.minNlp;
+              obj.hyp.plsor = minx(1:length(obj.hyp.plsor));
+              obj.hyp.cov = minx(length(obj.hyp.plsor)+1:end-1);
+              obj.hyp.sigma2 = minx(end);
+
+              obj.minNlp = miny;
+              obj.OptimInfo = optinfo;
+              obj.OptimExitFlag = exitflag;
+              obj.OptimTrial = i;
+            end
+          end
+        case 'none'
+          return;
+        otherwise
+          error('Fit method ''%s'' not supported', obj.fitMethod);
+      end
     end
 
   end
