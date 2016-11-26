fd5f515d3d4ec940eed9825d68ad5928b6edcfb0
jakub <j.repicky@gmail.com>
2016-11-27 00:19:34 +0100

    Polish off the synthetic tests.

--- scripts/synthTest.m
+++ scripts/synthTest.m
@@ -1,22 +1,19 @@
-%% Experiments on the PLSOR method.
-
-% Initialize the rng.
-seed = 5;
+% Initialize the random number generator for reproducibility.
+seed = 2;
 rng(seed);
 
-%% Generate test data.
+%% Training data
 % The PLSOR model is trained on a small sample of data from interval
 % [0, 20]:
-% - 2 random points from interval [2, 4] in category 1
-% - 3 random points from interval [4, 6] in category 2
-% - 10 points from [8, 14] in category 3
-% - 5 points from [14, 18] in category 4
-% - 1 point from [18, 20] in category 5
 %
-% The categories are defined by c(y) = floor(y).
+% * 2 random points from interval [2, 4] in category 1
+% * 3 points from [4, 6] in category 2
+% * 10 points from [8, 14] in category 3
+% * 5 points from [14, 18] in category 4
+% * 1 point from [18, 20] in category 5
 %
-% The model's response is tested on linearly spaced points between
-% 0 and 20.
+% The target values for each category are sampled from a uniform
+% distribution with mean at the category.
 
 intrvs = [2 4; 4 6; 8 14; 14 18; 18 20];
 freqs = [2 3 10 5 1];
@@ -26,43 +23,59 @@ Xtr = zeros(sum(freqs), 1);
 ytr = zeros(sum(freqs), 1);
 
 for i = 1:size(intrvs, 1)
-  Xtr(es(i)+1:es(i+1)) = intrvs(i, 1) + range(intrvs(i, :)) * rand(1, freqs(i));
-  ytr(es(i)+1:es(i+1)) = bins(i) + rand(1, freqs(i));
+  Xtr(es(i)+1:es(i+1)) = intrvs(i, 1) + range(intrvs(i, :)) * ...
+    rand(1, freqs(i));
+  ytr(es(i)+1:es(i+1)) = bins(i) + rand(1, freqs(i)) - 0.5;
 end
 
-ytrBins = floor(ytr);
+%% Fitting the model
+% The model is fitted on ordinal values. In this case the output space
+% is discretized by rounded off the targets.
+%
+% Fitting is done by creating an instance of |OrdRegressionGP| class.
+Ytr = round(ytr);
 
-Xte = linspace(0, 20, 200)';
-
-%% Train the model and show some of its properties.
 tic;
-ordgp = OrdRegressionGP(Xtr, ytrBins);
+ordgp = OrdRegressionGP(Xtr, Ytr);
 t1 = toc;
 
-disp('Elapsed time:');
+%% Model's properties
+% The created object contains optimized hyperparameters and other state
+% variables required for predictions.
+%
+% Here, the fitting time, the successful fitting trial, the optimal
+% pseudo-likelihood (negative log predicted probability) reached,
+% and the fitted hyperparameter values are shown.
+disp('Fitting time:');
 disp(t1);
+disp('The successful trial:');
+disp(ordgp.OptimTrial);
 disp('Minimal negative log probability:');
 disp(ordgp.MinimumNLP);
 disp('PLSOR hyperparameters:');
 disp(ordgp.PlsorParameters);
 disp('Covariance hyperparameters:');
 disp(ordgp.KernelParameters);
-disp('Sigma2:');
+disp('The noise hyperparameter:');
 disp(ordgp.Sigma2);
-disp('The best starting point:');
-disp(ordgp.OptimTrial);
 
-%% Make predictions.
+%% Making predictions
+% The model's response is tested on linearly spaced points between
+% 0 and 20.
+%
+% Prediction is implemented by the |predict| method of an |OrdRegressionGP|
+% object.
+%
 % The first output argument contains the most probable ordinal class for
-% each test point. The second argument are the predicted probabilities.
-% The third and fourth are the mean and variance of the latent variable
-% predicted by the underlying Gaussian process.
+% each test point. The second output argument contains the predicted
+% probabilities. The mean and variance by the underlying Gaussian process
+% are returned in the third and fourth arguments, respectively.
+Xte = linspace(0, 20, 200)';
 [y, probs, mu, s2] = ordgp.predict(Xte);
 
-%% *Figure 1:* Gaussian process and PLSOR predictions.
-% Show the predicted mean and variance of the latent variables
-% and classification on some test points.
-
+%% *Figure 1:* Gaussian process and PLSOR predictions
+% Plot the predicted mean and variance of the latent variable
+% and predicted classes of a uniformely spaced selection of test points.
 plot(Xtr, ytr, '+');
 hold on;
 grid on;
@@ -72,5 +85,7 @@ plot(Xte, mu, 'r-');
 plot(Xte, mu - sqrt(s2), 'b--');
 plot(Xte, mu + sqrt(s2), 'b--');
 plot(Xte(10:10:end), y(10:10:end), 'ro');
-legend('Training data', 'Mean', '+ std', '- std', 'Predicted class', ...
+legend('Training data', 'Predicted mean', 'mu + std', 'mu - std', 'Test predictions', ...
   'Location', 'southeast');
+snapnow;
+close all;
