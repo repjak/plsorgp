2d43dd4c4b85953d26f8332b6ffb9ba18e7fbc7f
FILL_ME <FILL_ME>
2017-04-29 13:47:38 +0200

    Update metacentrum task to PBS PRO.

--- metacentrum_task.sh
+++ metacentrum_task.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #PBS -N plsorgp_benchmark
-#PBS -l nodes=1#excl
-#PBS -l mem=1gb
-#PBS -l scratch=100mb
+#PBS -l select=1:ncpus=8:mem=1gb:scratch_local=100mb
+#PBS -l place=excl
 #PBS -l matlab=1
 #PBS -l matlab_Optimization_Toolbox=1
 #PBS -l matlab_Statistics_Toolbox=1
-#PBS -l walltime=1h
+#PBS -l walltime=2:00:00
 #PBS -j oe
 #PBS -m e
 
